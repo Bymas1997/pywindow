5bf48f5aae50a9aa701117c2549b8343e1fec234
Bymas-handaotech <kimhyunkestu97@gmail.com>
2022-06-20 16:43:09 +0800

    2022.6.20.2

---
+++ sensors/abstract.py
@@ -0,0 +1,212 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import typing as t
+from abc import abstractmethod
+from collections import OrderedDict
+from threading import Thread
+from threading import Condition as ThreadCondition
+from queue import Queue as ThreadQueue
+from multiprocessing import Process
+from multiprocessing import Condition as ProcessCondition
+from multiprocessing import Queue as ProcessQueue
+from .utils import Scheduler
+
+
+class AbstractThreadSensor(Thread):
+    """ Abstract thread sensor object, which serves as a child class of a Thread. """
+
+    def __init__(
+        self,
+        receiver: t.Optional[t.Callable] = None,
+        mode: str = "live",
+        fps: int = 30,
+        data_queue: t.Optional[ThreadQueue] = None,
+        result_queue: t.Optional[ThreadQueue] = None,
+        add_condition: bool = True,
+        connect_while_init: bool = True,
+        name: t.Optional[str] = None,
+        **options
+    ) -> None:
+        super(AbstractThreadSensor, self).__init__()
+        self._receiver: t.Optional[Callable] = receiver
+        self._mode = mode  # Sensor's mode, "live" or "trigger".
+        self._fps = fps  # Maximal refreshing frequency while in live mode.
+        self._data_queue = data_queue
+        self._result_queue = result_queue
+        self._name = name or "thread_sensor"
+        self._scheduler = Scheduler(max_fps=fps)
+        self._options = options
+
+        if add_condition:
+            self._condition = ThreadCondition()
+            self._condition.acquire()
+        else:
+            self._condition = None
+        if connect_while_init:
+            self.connect()
+
+    @property
+    def name(self) -> str:
+        """ Returns the worker's name. """
+        return self._name
+
+    @property
+    def data_queue(self) -> ThreadQueue:
+        """ Returns the thread's data queue. """
+        return self._data_queue
+
+    @property
+    def result_queue(self) -> ThreadQueue:
+        """ Returns the thread's result queue. """
+        return self._result_queue
+
+    @abstractmethod
+    def connect(self, **options) -> bool:
+        """ Connecting ot the sensor device, returns whether the device has been connected. """
+        pass
+
+    @abstractmethod
+    def disconnect(self, **options) -> bool:
+        """ Disconnecting ot the sensor device, returns whether the device has been disconnected. """
+        pass
+
+    @abstractmethod
+    def is_connected(self) -> bool:
+        """ Returns whether the sensor device has been correctly connected. """
+        pass
+
+    @abstractmethod
+    def trigger(self, **options) -> bool:
+        """ Trigger the sensor while in trigger mode, it returns whether the sensor has been triggered. """
+        pass
+
+    @abstractmethod
+    def wait(self, timeout: t.Optional[float] = None, **options) -> None:
+        """ Block the thread. """
+        pass
+
+    @abstractmethod
+    def notify(self, **options) -> None:
+        """ Wake up the thread. """
+        pass
+
+    @abstractmethod
+    def exit(self, **options) -> None:
+        """ Function for safely exit the thread running loop. """
+        pass
+
+
+class AbstractProcessSensor(Process):
+    """ Abstract process sensor object, which serves as a child class of a Process. """
+
+    def __init__(
+        self,
+        receiver: t.Optional[t.Callable] = None,
+        mode: str = "live",
+        fps: int = 30,
+        data_queue: t.Optional[ProcessQueue] = None,
+        result_queue: t.Optional[ProcessQueue] = None,
+        add_condition: bool = True,
+        connect_while_init: bool = True,
+        name: t.Optional[str] = None,
+        **options
+    ) -> None:
+        super(AbstractProcessSensor, self).__init__()
+        self._receiver: t.Optional[t.Callable] = receiver
+        self._mode = mode  # Sensor's mode, "live" or "trigger".
+        self._fps = fps    # Maximal refreshing frequency while in live mode.
+        self._data_queue = data_queue
+        self._result_queue = result_queue
+        self._name = name or "process_sensor"
+        self._config_cache: t.Dict[str, t.Dict[str, t.Any]] = OrderedDict()
+        self._scheduler = Scheduler(max_fps=fps)
+        self._options = options
+
+        if data_queue is None:
+            self._data_queue = ProcessQueue(maxsize=16)
+        if result_queue is None:
+            self._result_queue = ProcessQueue(maxsize=1)
+        if add_condition:
+            self._condition = ProcessCondition()
+            self._condition.acquire()
+        else:
+            self._condition = None
+        if connect_while_init:
+            self.connect()
+
+    @property
+    def name(self) -> str:
+        """ Returns the worker's name. """
+        return self._name
+
+    @property
+    def data_queue(self) -> ThreadQueue:
+        """ Returns the process data queue. """
+        return self._data_queue
+
+    @property
+    def result_queue(self) -> ThreadQueue:
+        """ Returns the process result queue. """
+        return self._result_queue
+
+    @abstractmethod
+    def connect(self, **options) -> bool:
+        """ Connecting ot the sensor device, returns whether the device has been connected. """
+        pass
+
+    @abstractmethod
+    def disconnect(self, **options) -> bool:
+        """ Disconnecting ot the sensor device, returns whether the device has been disconnected. """
+        pass
+
+    @abstractmethod
+    def is_connected(self) -> bool:
+        """ Returns whether the sensor device has been correctly connected. """
+        pass
+
+    @abstractmethod
+    def trigger(self, **options) -> bool:
+        """ Trigger the sensor while in trigger mode, it returns whether the sensor has been triggered. """
+        pass
+
+    @abstractmethod
+    def wait(self, timeout: t.Optional[float] = None, **options) -> None:
+        """ Block the process. """
+        pass
+
+    @abstractmethod
+    def notify(self, **options) -> None:
+        """ Wake up the process. """
+        pass
+
+    @abstractmethod
+    def exit(self, **options) -> None:
+        """ Function for safely exit the process running loop. """
+        pass
+
+    def _cache_config(self, configuration: t.Union[t.Dict[str, t.Any], t.List[t.Dict[str, t.Any]]]) -> None:
+        """ Cache configs into the config cache, this function is to be called inside the process running loop. """
+        if isinstance(configuration, dict):
+            self._cache_single_config(configuration)
+        elif isinstance(configuration, list):
+            for cfg in configuration:
+                self._cache_single_config(cfg)
+
+    def _cache_single_config(self, configuration: t.Dict[str, t.Any]):
+        """ Cache a single piece of configuration, the configuration dictionary should have the following format:
+            configuration = {
+                "module": str
+                "key": str,
+                "value": any,
+                "index": int,
+                "name": str
+            }
+        """
+        key, value, module = configuration.get("key"), configuration.get("value"), configuration.get("module")
+        if module is None:
+            config_key = key
+        else:
+            config_key = module + '-' + key
+        self._config_cache[config_key] = configuration
+
---
+++ sensors/fake_sensor.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import copy
+import random
+import typing as t
+from queue import Queue as ThreadQueue
+from .abstract import AbstractThreadSensor
+
+
+class FakeSensor(AbstractThreadSensor):
+    """ Fake sensor object. """
+
+    def __init__(
+        self,
+        receiver: t.Optional[t.Callable] = None,
+        mode: str = "trigger",
+        fps: int = 30,
+        data_queue: t.Optional[ThreadQueue] = None,
+        result_queue: t.Optional[ThreadQueue] = None,
+        add_condition: bool = True,
+        connect_while_init: bool = True,
+        name: t.Optional[str] = None,
+        **options
+    ) -> None:
+        if data_queue is None:
+            data_queue = ThreadQueue(maxsize=16)
+        if result_queue is None:
+            result_queue = ThreadQueue(maxsize=1)
+        self._is_connected: bool = False
+        super(FakeSensor, self).__init__(receiver, mode, fps, data_queue, result_queue, add_condition,
+                                         connect_while_init, name, **options)
+        self._data = self._load_data(options.get("data_path"))
+        self._is_triggered: bool = False or self._mode == "live"
+        self._is_exit: bool = False
+        self.start()
+
+    @staticmethod
+    def _load_data(data_path: t.Optional[str]) -> t.List[t.List[float]]:
+        """ Load the input data into a python list. If the file not exists, returns the fake data list. """
+        data_list: t.List[t.List[float]] = list()
+        if data_path is None or not os.path.isfile(data_path):
+            intervals: int = 1000
+            for i in range(intervals):
+                data_list.append([-8.0 + i * 16.0 / intervals, 0.0])
+        else:
+            with open(data_path, 'r') as f:  # 以只读形式打开某.txt文件
+                for line in f:
+                    line = line.strip('\n')  # 去掉换行符
+                    line = line.split('\t')  # 分割掉两列数据之间的制表符
+                    data_list.append([float(line[0]), float(line[1])])
+                f.close()
+
+        return data_list
+
+    def connect(self, **options) -> bool:
+        """ Connecting ot the sensor device, returns whether the device has been connected. """
+        self._is_connected = True
+        return self._is_connected
+
+    def disconnect(self, **options) -> bool:
+        """ Disconnecting ot the sensor device, returns whether the device has been disconnected. """
+        self._is_connected = False
+        return self._is_connected
+
+    def is_connected(self) -> bool:
+        """ Returns whether the sensor device has been correctly connected. """
+        return self._is_connected
+
+    def trigger(self, **options) -> bool:
+        """ Trigger the sensor while in trigger mode, it returns whether the sensor has been triggered. """
+        if not self._is_connected or self._mode != "trigger" or self._is_triggered:
+            return False
+        self._data_queue.put({"trigger": True})
+        return True
+
+    def wait(self, timeout: t.Optional[float] = None, **options) -> None:
+        """ Block the thread. """
+        self._data_queue.put({"wait": timeout})
+
+    def notify(self, **options) -> None:
+        """ Wake up the thread. """
+        self._condition.notify(**options)
+
+    def exit(self, **options) -> None:
+        """ Function for safely exit the thread running loop. """
+        self.disconnect()
+        if self._data_queue.full():
+            self._data_queue.get()
+        self._data_queue.put({"exit": True})
+        while self.is_alive() and self.result_queue.full():
+            self._result_queue.get()
+
+    def run(self) -> None:
+        """ Definition for running the sensor thread looping. """
+        num_data = len(self._data)
+        crop_range = int(0.15 * num_data)
+
+        while True:
+            if self._pre_looping_condition():
+                data = self._data_queue.get()
+                if "trigger" in data or self._mode == "live":
+                    self._is_triggered = True
+                elif "wait" in data:
+                    self._condition.wait(data["wait"])
+                elif "config" in data:
+                    pass
+                elif "exit" in data:
+                    break
+
+            if not self._is_triggered:
+                self._scheduler()
+                continue
+            front_index: int = random.randint(0, crop_range)
+            end_index: int = random.randint(num_data-crop_range, num_data)
+            data = copy.deepcopy(self._data[front_index:end_index])
+            if self._receiver is not None:
+                self._receiver(data)
+            else:
+                self._result_queue.put({"data": data})
+
+            self._is_triggered = self._mode == "live"
+            self._scheduler()
+
+    def _pre_looping_condition(self) -> bool:
+        """ Condition before each loop. """
+        if self._mode == "live":
+            return not self._data_queue.empty()
+        elif self._mode == "trigger":
+            return True
+        else:
+            raise ValueError("Unsupported camera worker mode.")
