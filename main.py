23fbdf437768881c9b30728de9a45a62c663a371
Bymas-handaotech <kimhyunkestu97@gmail.com>
2022-06-24 16:33:24 +0800

    2022.6.24

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from PySide2.QtWidgets import QApplication, QLabel, QMainWindow, QGraphicsScene, QTableWidgetItem, QHeaderView, \
     QFileDialog
 from PySide2.QtGui import QIcon
+from PySide2.QtCore import QTimer
 from ip_settings.system_settings import Widget
 from main_ui import Ui_MainWindow
 from graph import MyFigureCanvas
@@ -11,6 +12,7 @@ import matplotlib.pyplot as plt
 from data import myData
 import numpy as np
 import xlwt
+import random
 
 # from sensor_test import show_result
 
@@ -21,8 +23,10 @@ class Stats(QMainWindow):
 
     def __init__(self):
         super(Stats, self).__init__()
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.func1.setEnabled(False)
 
         self.area_x = []
         self.area_y = []
@@ -34,7 +38,7 @@ class Stats(QMainWindow):
         self.y = None
         self.xlim = None  # 用来存储左边原始图像的x坐标轴范围
         self.ylim = None  # 用来存储左边原始图像的y坐标轴范围
-        self.distance = []
+        self.distance = None
 
         self.gv_visual_data_content = MyFigureCanvas(width=self.ui.graphicsView.width() / 101,
                                                      height=self.ui.graphicsView.height() / 101,
@@ -46,6 +50,15 @@ class Stats(QMainWindow):
                                                       ylim=(-9, 9))  # 实例化一个FigureCanvas
         self.graphic_scene = QGraphicsScene()  # 创建一个QGraphicsScene
         self.graphic_scene1 = QGraphicsScene()  # 创建一个QGraphicsScene(右边局部)
+        self.graphic_scene.addWidget(self.gv_visual_data_content)
+        # 把图形放到QGraphicsScene中，注意：图形是作为一个QWidget放到放到QGraphicsScene中的
+        self.graphic_scene1.addWidget(self.gv_visual_data_content1)
+
+        self.ui.graphicsView.setScene(self.graphic_scene)  # 把QGraphicsScene放入QGraphicsView
+        self.ui.graphicsView_2.setScene(self.graphic_scene1)  # 把QGraphicsScene放入QGraphicsView(右边局部)
+
+        self.ui.graphicsView.show()  # 调用show方法呈现图形
+        self.ui.graphicsView_2.show()  # 调用show方法呈现图形
         self.rect = plt.Rectangle((0, 0), 0, 0, color="springgreen", alpha=0.2)  # 选取的范围框
         self.tool = QLabel(self)  # 实例化一个标签，用来作为工具栏的容器
         self.tool.hide()  # 隐藏工具栏
@@ -69,12 +82,75 @@ class Stats(QMainWindow):
         # self.ui.realtime_monitor.clicked.connect(show_result)
         self.ui.setting.clicked.connect(self.system_settings)
         self.ui.back.clicked.connect(self.back)
-
+        self.ui.realtime_monitor.clicked.connect(self.monitor)
         self.ui.Traking.setEnabled(False)
+        if self.distance is None:
+            self.ui.func1.setEnabled(True)
 
-    def system_settings(self):
-        self.ss = Widget()
-        self.ss.show()
+    def monitor(self):
+
+        filename, _ = QFileDialog.getOpenFileName(self, caption="选择文件", dir=os.getcwd(), filter="(*txt)")
+        mydata = myData(filename)
+        self.x = mydata.x
+        self.y = mydata.y
+        self._plot_ref = None
+        self.i = 1  # cln画法 i= 0
+
+        self.update_plot()
+        # self.show()
+        self.timer = QTimer()
+        self.timer.setInterval(2)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
+        self.ui.realtime_monitor.clicked.connect(self.timer.stop)
+
+    def update_plot(self):
+
+        """
+               Clear and redraw&In-place redraw
+               """
+        # TODO:
+        # self.gv_visual_data_content.axes.cla()
+        # self.gv_visual_data_content.axes.set_ylim(-8, 8)
+        # self.gv_visual_data_content.axes.set_xlim(-8, 8)
+        #
+        # self.i += 1
+        # self.xdata =self.x[0:self.i]
+        # self.ydata =self.y[0:self.i]
+        # self.ydata = self.ydata + [random.randint(0, 10)]
+        #
+        # print(self.xdata, self.ydata)
+        # self.gv_visual_data_content.axes.plot(self.xdata,self.ydata)
+        # self.gv_visual_data_content.draw_idle()
+
+        """
+               以上为Clear and redraw画法 
+               """
+        # TODO:
+        self.gv_visual_data_content.axes.set_ylim(-8, 8)
+        self.gv_visual_data_content.axes.set_xlim(-8, 8)
+
+        self.i += 1
+
+        if self._plot_ref is None:
+            self.xdata = self.x[0:1]
+            self.ydata = self.y[0:1]
+            plot_refs = self.gv_visual_data_content.axes.plot(self.xdata, self.ydata, 'r')
+            self._plot_ref = plot_refs[0]
+        else:
+
+            print(self.i)
+            self.xdata.append(self.x[self.i])
+            self.ydata.append(self.y[self.i])
+            self._plot_ref.set_xdata(self.xdata)
+            self._plot_ref.set_ydata(self.ydata)
+
+        print(self.xdata)
+
+        self.gv_visual_data_content.draw()
+        """
+                      以上为In-place redraw画法 
+                      """
 
     def file(self):  # 文件读取
         self.gv_visual_data_content.axes.cla()
@@ -86,35 +162,25 @@ class Stats(QMainWindow):
 
         self.plot_data()
 
+    def system_settings(self):
+        self.ss = Widget()
+        self.ss.show()
+
     def plot_data(self):
 
         self.gv_visual_data_content.axes.set_ylim(-8, 8)
         self.gv_visual_data_content1.axes.set_ylim(-8, 8)
-
         self.gv_visual_data_content.axes.plot(self.x, self.y)
-        self.gv_visual_data_content.draw_idle()  # 此行代码至关重要，若没有改行代码，右边图像将无法随矩形选区更新，改行代码起实时更新作用
-
         self.gv_visual_data_content1.axes.plot(self.x, self.y)
-        self.gv_visual_data_content1.draw_idle()  # 此行代码至关重要，若没有改行代码，右边图像将无法随矩形选区更新，改行代码起实时更新作用
-
         self.gv_visual_data_content.axes.add_patch(self.rect)  # 添加选框范围矩形到画布, 这里需要先初始化一个矩形，后面只需对该矩形矩形参数重设即可让矩形动起来。
         self.xlim = self.gv_visual_data_content.axes.get_xlim()  # 获取原始x轴范围
         self.ylim = self.gv_visual_data_content.axes.get_ylim()  # 获取原始y轴范围
-        self.gv_visual_data_content.axes.set_title('')
-        # 加载的图形（FigureCanvas）不能直接放到graphicview控件中，必须先放到graphicScene，然后再把graphicscene放到graphicview中
-        self.graphic_scene.addWidget(self.gv_visual_data_content)
-        # 把图形放到QGraphicsScene中，注意：图形是作为一个QWidget放到放到QGraphicsScene中的
-        self.graphic_scene1.addWidget(self.gv_visual_data_content1)
-
-        self.ui.graphicsView.setScene(self.graphic_scene)  # 把QGraphicsScene放入QGraphicsView
-        self.ui.graphicsView_2.setScene(self.graphic_scene1)  # 把QGraphicsScene放入QGraphicsView(右边局部)
-
-        self.ui.graphicsView.show()  # 调用show方法呈现图形
-        self.ui.graphicsView_2.show()  # 调用show方法呈现图形
         self.gv_visual_data_content.toolbar.mode = "zoom rect"  # 将隐藏的画图工具栏的选框放大模式打开
         self.gv_visual_data_content.mpl_connect('button_release_event', self.release)  # 在原始画布上的鼠标按下事件
         self.gv_visual_data_content.mpl_connect('button_press_event', self.press)  # 在原始画布上的鼠标释放事件， 该事件绑定了关键操作
         self.ui.func1.setEnabled(True)
+        self.gv_visual_data_content.draw_idle()
+        self.gv_visual_data_content1.draw_idle()
 
     def press(self, event):  # 鼠标在原始画布上按下将会被调用的函数，该函数获取鼠标按下的坐标
         self.startx = event.x
@@ -174,6 +240,7 @@ class Stats(QMainWindow):
             dataxy = str(cu_datax) + '  ' + str(cu_datay) + '\n' + '\n'  # text函数转换数字类型至字符串打印
             self.gv_visual_data_content1.axes.text(cu_dataxx, cu_datayy, dataxy)  # 打印选定数据点
             self.gv_visual_data_content1.axes.plot(cu_dataxx, cu_datayy, '.r')
+            print(cu_dataxx, cu_datayy)
             self.gv_visual_data_content1.draw_idle()  # 此行代码至关重要，若没有改行代码，右边图像将无法随矩形选区更新，改行代码起实时更新作用
             self.marked_x.append(cu_dataxx)
             self.marked_y.append(cu_datayy)
@@ -205,6 +272,8 @@ class Stats(QMainWindow):
         self.marked_data = [i for i in zip(self.x, self.y) if
                             bigger_x >= i[0] >= smaller_x and bigger_y >= i[1] >=
                             -999]
+        if self.distance is not None:
+            self.ui.func1.setEnabled(False)
 
     def _area(self):  # func3
         for num in self.marked_data:
@@ -233,10 +302,11 @@ class Stats(QMainWindow):
         depth_point = self.marked_data[max_depth_index]
         drop_foot_x, drop_foot_y = np.linalg.solve(
             [[self.marked_y[0] - self.marked_y[1], self.marked_x[1] - self.marked_x[0]],
-             [-(self.marked_x[1]-self.marked_x[0]) / (self.marked_y[0] - self.marked_y[1]), 1]],
+             [-(self.marked_x[1] - self.marked_x[0]) / (self.marked_y[0] - self.marked_y[1]), 1]],
             [self.marked_x[1] * self.marked_y[0] - self.marked_x[0] * self.marked_y[1],
-             (-(depth_point[0]*(self.marked_x[1]-self.marked_x[0])) / (self.marked_y[0] - self.marked_y[1])) + depth_point[1]])
-        self.gv_visual_data_content1.axes.plot([depth_point[0],drop_foot_x], [depth_point[1], drop_foot_y])
+             (-(depth_point[0] * (self.marked_x[1] - self.marked_x[0])) / (self.marked_y[0] - self.marked_y[1])) +
+             depth_point[1]])
+        self.gv_visual_data_content1.axes.plot([depth_point[0], drop_foot_x], [depth_point[1], drop_foot_y])
         print(depth_point)
         print(drop_foot_x, drop_foot_y)
         depth_plot = 'd :' + str(self.depth_max)
@@ -258,7 +328,7 @@ class Stats(QMainWindow):
 
         # self.xlim = None  # 用来存储左边原始图像的x坐标轴范围
         # self.ylim = None  # 用来存储左边原始图像的y坐标轴范围
-        self.distance = []
+        self.distance = None
 
     def delete(self):
         self.ui.widget_result.clearContents()
@@ -268,6 +338,8 @@ class Stats(QMainWindow):
         self.gv_visual_data_content1.axes.set_ylim(self.rect_y[0], self.rect_y[1])
         self.gv_visual_data_content1.draw_idle()
         self.cln_data()
+        if self.distance is None:
+            self.ui.func1.setEnabled(True)
 
     def save(self):
 
@@ -298,7 +370,7 @@ class Stats(QMainWindow):
             self.gv_visual_data_content1.draw_idle()  # 此行代码至关重要，若没有改行代码，右边图像将无法随矩形选区更新，改行代码起实时更新作用
             self.area_x = []
             self.area_y = []
-            self.distance = []
+            self.distance = None
         elif len(self.marked_x) == 1:
             del self.marked_x[0]
             del self.marked_y[0]
@@ -308,6 +380,8 @@ class Stats(QMainWindow):
             self.gv_visual_data_content1.axes.set_ylim(self.rect_y[0], self.rect_y[1])
             self.gv_visual_data_content1.axes.plot(self.x, self.y)
             self.gv_visual_data_content1.draw_idle()  # 此行代码至关重要，若没有改行代码，右边图像将无法随矩形选区更新，改行代码起实时更新作用
+        if self.distance is None:
+            self.ui.func1.setEnabled(True)
 
 
 if __name__ == '__main__':
